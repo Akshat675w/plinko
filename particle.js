63d08202359fcaa145418efa8e74202434625154
Akshat675w <77610659+Akshat675w@users.noreply.github.com>
2021-07-20 13:43:24 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,25 @@
+class Particle{
+    constructor(x,y,r){
+        Option={
+           restitution : 1,
+           friction : 0
+        }
+
+        this.body = Bodies.circle(x,y,r,Option)
+        this.color = color( random(0,255) , random(0,255), random(0,255))
+        World.add(world,this.body)
+   }
+ 
+ display(){
+    push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+ }
+
+}
